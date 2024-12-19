615e8434419426ede2383779cccbf42e758de1ed
David35798 <dvjmj35798@gmail.com>
2024-12-20 01:35:34 +0900

    First commit in devB branch

--- MyGitTest2/src/mygit/B.java
+++ MyGitTest2/src/mygit/B.java
@@ -1,6 +1,10 @@
 package mygit;
 
 public class B {
+	public void say(String thisClass) {
+		System.out.println("This is B and " + thisClass);
+	}
+
 	public static void main(String[] args) {
 		System.out.println("This is B.java");
 	}
