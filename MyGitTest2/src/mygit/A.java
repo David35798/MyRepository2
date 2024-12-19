2176ddeec4f7f09aa4ae83493f332c7ef4314116
David35798 <dvjmj35798@gmail.com>
2024-12-20 01:41:39 +0900

    First commit in devC branch

--- MyGitTest2/src/mygit/A.java
+++ MyGitTest2/src/mygit/A.java
@@ -3,5 +3,8 @@ package mygit;
 public class A {
 	public static void main(String[] args) {
 		System.out.println("This is A.java");
+		System.out.println("This is devC");
+		C c = new C();
+		c.say("A");
 	}
 }
