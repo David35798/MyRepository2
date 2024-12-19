083ae02a5d46475847760835f3e963e4dc984f80
David35798 <dvjmj35798@gmail.com>
2024-12-20 01:34:54 +0900

    First commit in devB branch

--- MyGitTest2/src/mygit/A.java
+++ MyGitTest2/src/mygit/A.java
@@ -3,5 +3,8 @@ package mygit;
 public class A {
 	public static void main(String[] args) {
 		System.out.println("This is A.java");
+		System.out.println("This is devB");
+		B b = new B();
+		b.say("A");
 	}
 }
